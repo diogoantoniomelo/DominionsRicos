d343ff84f32ad4d74e8c920230ab4894830fa18e
Diogo Melo <diogo.antoniomelo@gmail.com>
2023-08-17 23:23:30 -0300

    Definindo as Entidades, Corrupção no Código e Primitive Obsession

---
+++ PaymentContext.Domain/Entities/PayPalPayment.cs
@@ -0,0 +1,25 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class PayPalPayment : Payment
+    {
+        public PayPalPayment(
+            string transactionCode,
+            DateTime paidDate,
+            DateTime expireDate,
+            decimal total,
+            decimal totalPaid,
+            string payer,
+            string document,
+            string adress,
+            string email
+        )
+            : base(paidDate, expireDate, total, totalPaid, payer, document, adress, email)
+        {
+            TransactionCode = transactionCode;
+        }
+
+        public string TransactionCode { get; private set; }
+    }
+}
